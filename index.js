b9a5cb345f4be5eeab05a8ddf7d05c80b3ebb86d
Salihcan Özdemir <105110588+salihcanozd@users.noreply.github.com>
2024-09-24 12:51:25 +0000

    password project

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
 import express from "express";
-import parser from "body-parser";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(parser.urlencoded({extended:true}));
+var userIsAuthorised = false;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req,res,next){
+
+    if(req.body["password"]=="password"){
+        userIsAuthorised = true;
+
+    }
+
+
+    next();
+}
+app.use(passwordCheck);
+
 
 app.get("/",(req,res)=>{
-    res.sendFile("/workspaces/ExpressJs/public/index.html")
-})
+    res.sendFile(__dirname+"/public/index.html");
+});
 
-app.post("/submit",(req,res)=>{
-    console.log(req.body);
-
-    res.send("About Page");
+app.post("/check",(req,res)=>{
+    if(userIsAuthorised){
+        res.sendFile(__dirname+"/public/secret.html");
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 
-
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`)
-});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
